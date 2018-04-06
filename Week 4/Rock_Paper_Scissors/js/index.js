14b34026cd9b7323d1022ce6d430e698f6bf26d8
Philippine du Merac <philippine.merac@gmail.com>
2018-04-05 21:30:42 -0700

    push

--- Week 4/Rock_Paper_Scissors/js/index.js
+++ Week 4/Rock_Paper_Scissors/js/index.js
@@ -1,8 +1,39 @@
+console.log("js loaded")
 
-document.getElementById('rock').onclick = weaponRock
+$("#weblink").click(webLinkClicked)
 
-function weaponRock(){
-	console.log("You chose rock or whatever")
-	console.log("Computer player chooses paper")
-	console.log("LOST!!!!")
+$("#rock").click (rockButtonClicked)
+
+$("#paper").click (paperButtonClicked)
+
+$("#scissors").click (scissorsButtonClicked)
+
+function rockButtonClicked(){
+    console.log("You chose rock")
+    console.log("Computer player chooses rock")
+    console.log("It's a tie")
+    $("body").css("background-color","yellow")
+    $("#rock").fadeOut()
+     $("#paper").fadeIn()
+}
+
+function paperButtonClicked(){
+    console.log("You chose paper")
+    console.log("Computer player chooses rock")
+    console.log("You win!!!")
+    $("body").css("background-color","pink")
+    $("#paper").fadeOut()
+    $("#rock").fadeIn()
+}
+
+function scissorsButtonClicked(){
+    console.log("You chose scissors")
+    console.log("Computer player chooses rock")
+    console.log("You lose")
+    $("body").css("background-color","blue")
+
+}
+
+function webLinkClicked(){
+    alert("Woohoo you clicked the secret link!")
 }
