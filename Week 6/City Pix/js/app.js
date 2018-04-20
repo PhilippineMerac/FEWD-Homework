c8da1aea6e14836d3baf0767e1aa94ba36bf009d
Philippine du Merac <philippine.merac@gmail.com>
2018-04-19 19:12:44 -0700

    push

--- Week 6/City Pix/js/app.js
+++ Week 6/City Pix/js/app.js
@@ -1,23 +1,55 @@
 $(document).ready(function(){
 
-	function updateCity() {
-		// grab the value in the city text box, store it as city variable
-		var city = $("city-type").val()
-		
 
-		console.log("city submitted")
 
-		if (city=="la"){
-		$("body").attr("class","la")
-		}
+    function updateCity(){
+        // grab the value in the city text box, store it as the city variable:
+        var city = $("#city-type").val()
 
-		$("city-type").val("")
+        // output city to the js console:
+        console.log(city)
 
-		// precent the defaulty action after form submit (page reload):
-		event.preventDefault()
-		// code goes here
-	}
+        // if city is "la", change the backgrond to images/la.jpg:
+        if(city == "la"){
+            $("body").attr("class", "la")
+        }
 
-	$("#city-form").submit(updateCity)
+        if(city == "nyc"){
+            $("body").attr("class", "nyc")
+        }
 
-})
+        if(city == "sf"){
+            $("body").attr("class", "sf")
+        }
+
+        if(city == "austin"){
+            $("body").attr("class", "austin")
+        }
+
+        if(city == "paris"){
+            $("body").attr("class", "paris")
+        }
+
+        if(city == "sydney"){
+            $("body").attr("class", "sydney")
+        }
+
+        if(city == "tokyo"){
+            $("body").attr("class", "tokyo")
+        }
+
+        if(city == "london"){
+            $("body").attr("class", "london")
+        }
+
+        // blank out the text box:
+        $("#city-type").val("")
+
+        // prevent the default action after form submit (page reload):
+        event.preventDefault()
+    }
+
+    // Listen for a form submit event (enter or click button) and run updateCity when submitted:
+    $("#city-form").submit(updateCity)
+
+    })
