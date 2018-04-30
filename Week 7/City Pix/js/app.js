62f2248079b0d4ddc7122bac4700d6becfc02293
Philippine du Merac <philippine.merac@gmail.com>
2018-04-30 10:45:10 -0700

    push

--- Week 7/City Pix/js/app.js
+++ Week 7/City Pix/js/app.js
@@ -1,35 +1,33 @@
-var cities = ["nyc", "sf", "LA", "austin", "sydney","paris", "tokyo", "london"];
-$(document).ready(function(){
-    for(i=0;i<cities.length;i++) {
-    $('#city-type').append('<option value=" ' + cities[i] + ' ">' + cities[i] + '</option>');
+var cities = ["NYC", "SF", "LA", "ATX", "SYD","paris","london", "tokyo" ];
+$(document).ready(function() {
+  for(i=0;i<cities.length;i++) {
+    $('#city-type').append('<option value="' + cities[i] + '">' + cities[i] + '</option>');
   }
-
-  $('form').on('change','#city-type',function(){
+  $('form').on('change', '#city-type',function(){
     var city = $('#city-type').val();
-        // if city is "la", change the background to images/la.jpg:
-        if(city == 'la'){
-            $('body').attr('class', 'la')
-        }
-        else if(city == 'nyc'){
-            $('body').attr('class', 'nyc')
-        }
-        else if(city == 'sf'){
-            $('body').attr('class', 'sf')
-        }
-        else if(city == 'austin'){
-            $('body').attr('class', 'austin')
-        }
-        else if(city == 'paris'){
-            $('body').attr('class', 'paris')
-        }
-        else if(city == 'sydney'){
-            $('body').attr('class', 'sydney')
-        }
-        else if(city == 'tokyo'){
-            $('body').attr('class', 'tokyo')
-        }
-        else if(city == 'london'){
-            $('body').attr('class', 'london')
-        }
+    if(city == 'NYC') {
+      $('body').attr('class','nyc');
+    }
+    else if (city == 'SF') {
+      $('body').attr('class','sf');
+    }
+    else if (city == 'LA') {
+      $('body').attr('class','la');
+    }
+    else if (city == 'ATX') {
+      $('body').attr('class','austin');
+    }
+    else if (city == 'SYD') {
+      $('body').attr('class','sydney');
+    }
+    else if (city == 'paris') {
+      $('body').attr('class','paris');
+    }
+    else if (city == 'london') {
+      $('body').attr('class','london');
+    }
+    else if (city == 'tokyo') {
+      $('body').attr('class','tokyo');
+    }
   });
 });
