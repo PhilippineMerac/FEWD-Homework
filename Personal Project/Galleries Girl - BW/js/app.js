e54e5a54ca239900ccc97edb39d4405ade618cb9
Philippine du Merac <philippine.merac@gmail.com>
2018-05-15 20:44:53 -0700

    push

--- Personal Project/Galleries Girl - BW/js/app.js
+++ Personal Project/Galleries Girl - BW/js/app.js
@@ -1,6 +1,8 @@
-$ ("readmorelink").click(readMoreLinkClicked)
-
-function readMoreLinkClicked (){
-	console.log ("read More Link Clicked!")
-	event.preventDefault()
-}
+document.getElementsByTagName("li")[0].addEventListener("click",function(){
+	if(!this.parentNode.getAttribute("class")){
+		this.parentNode.setAttribute("class","slideDown");
+	}
+	else{
+		this.parentNode.removeAttribute("class");
+	}
+});
